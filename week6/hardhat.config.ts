f6b5977b0d6c23a695e280e2c149b32fde6b0b3f
ankitpal1029 <ankitkumarpal27@gmail.com>
2022-07-25 10:35:58 +0530

    basic

--- week6/hardhat.config.ts
+++ week6/hardhat.config.ts
@@ -1,8 +1,21 @@
+import("@nomiclabs/hardhat-ethers");
+import("@nomiclabs/hardhat-waffle");
+import dotenv from "dotenv";
+import { env } from "./lib/env";
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+const argv = JSON.parse(env("npm_config_argv"));
+if (argv.original !== ["hardhat", "test"]) {
+  require("dotenv").config();
+}
+
+import("./tasks/nft");
+
 import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.9",
+  solidity: "0.8.6",
 };
 
 export default config;
